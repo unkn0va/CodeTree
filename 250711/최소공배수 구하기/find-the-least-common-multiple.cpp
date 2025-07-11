e5b0a64a09f389e6c6240e604b60feac7b5a604c
unkn0va <lyunkn0vaa@gmail.com>
2025-07-11 14:11:35 +0900

    Title: 최소공배수 구하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250711/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+
+void num(int n, int m) {
+    int min_val = min(n, m);
+    int max_val = max(n, m);
+    int result;
+    for (int i = 2; i <= max_val; i++) {
+        result = min_val * i;
+        if (result % max_val == 0) {
+            cout << min_val;
+            break;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    num(n, m);
+    return 0;
+}
