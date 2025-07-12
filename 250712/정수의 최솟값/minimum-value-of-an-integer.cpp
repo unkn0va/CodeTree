72b2ea210e2f8737de52060c1d3f44dfcac0fcf5
unkn0va <lyunkn0vaa@gmail.com>
2025-07-12 12:48:04 +0900

    Title: 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250712/정수의 최솟값/minimum-value-of-an-integer.cpp
+++ 250712/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int a, b, c;
 
 int minInt(int a, int b, int c) {
-    if (a > b) {
+    if (a >= b) {
         if (b > c) return c;
         else return b;
     }
-    else if (b > a) {
+    else if (b >= a) {
         if (a > c) return c;
         else return a;
     }
-    else if (c > a) {
+    else if (c >= a) {
         if (a > b) return b;
         else return a;
     }
