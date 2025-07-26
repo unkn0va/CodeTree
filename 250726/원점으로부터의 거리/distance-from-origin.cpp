cd71bf0768721aa9fd020d2fd0e16fe237537a07
unkn0va <lyunkn0vaa@gmail.com>
2025-07-26 15:12:12 +0900

    Title: 원점으로부터의 거리, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250726/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int x[1000];
+int y[1000];
+
+class Spot {
+    public :
+        int x, y, number;
+
+        Spot(int x, int y, int number) {
+            this->x = x;
+            this->y = y;
+            this->number = number;
+        }
+
+        Spot() {}
+};
+
+bool cmp(Spot a, Spot b) {
+    return abs(a.x) + abs(a.y) < abs(b.x) + abs(b.y);
+}
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    // Please write your code here.
+    Spot spots[1005];
+
+    for (int j = 0; j < N; j++) {
+        spots[j] = Spot(x[j], y[j], j+1);
+    }
+    sort(spots, spots+N, cmp);
+
+    for (int j = 0; j < N; j++) {
+        cout << spots[j].number << endl;
+    }
+    return 0;
+}
