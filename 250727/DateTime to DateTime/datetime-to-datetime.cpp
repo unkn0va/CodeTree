b42d3d39b31fcb729a4e2828276b81e609e68e3a
unkn0va <lyunkn0vaa@gmail.com>
2025-07-27 17:22:35 +0900

    Title: DateTime to DateTime, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250727/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    int start, end;
+    start = 60*24*11 + 60*11 + 11;
+    end = 60*24*a + 60*b + c;
+    cout << end - start;
+    return 0;
+}
