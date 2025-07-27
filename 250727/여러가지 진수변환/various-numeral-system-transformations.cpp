8da67e0b3f79597db3e0ef36771969c8628cec99
unkn0va <lyunkn0vaa@gmail.com>
2025-07-27 19:45:23 +0900

    Title: 여러가지 진수변환, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250727/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N, B;
+
+int main() {
+    cin >> N >> B;
+
+    // Please write your code here.
+    int cnt = 0, binary[20] = {}, num = 0;
+    while (1) {
+        if (N < B) {
+            binary[cnt++] = N % 2;
+            break;
+        }
+        binary[cnt++] = N % B;
+        N /= B;
+    }
+
+    for (int i = cnt - 1; i >= 0; i--) {
+        cout << binary[i];
+    }
+    return 0;
+}
