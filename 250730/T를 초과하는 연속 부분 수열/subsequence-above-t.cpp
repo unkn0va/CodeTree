5b5899f37a06cd28cd45f64015f57330af1a4eb3
unkn0va <lyunkn0vaa@gmail.com>
2025-07-30 11:38:56 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250730/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+int arr[1000];
+
+int main() {
+    cin >> n >> t;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
