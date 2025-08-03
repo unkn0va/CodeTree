79d7dbe40930175260304df0b49d2db21ee7e631
unkn0va <lyunkn0vaa@gmail.com>
2025-08-03 14:45:48 +0900

    Title: 격자 위의 편안한 상태, Time: 14ms, Memory: 0MB, Status: Passed - Codetree

--- 250803/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 250803/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int N, M;
 int r[10000], c[10000];
 
+bool inRange(int a, int b) {
+    return a >= 0 && a <= N && b >= 0 && b < N;
+}
+
 int main() {
     cin >> N >> M;
     int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
@@ -16,9 +20,9 @@ int main() {
     // Please write your code here.
     for (int i = 0; i < M; i++) {
         int cnt = 0;
-        blocks[r[i]][c[i]] = 1;
+        blocks[r[i]-1][c[i]-1] = 1;
         for (int j = 0; j < 4; j++) {
-            if (blocks[r[i]+dx[j]][c[i]+dy[j]] == 1) cnt++;
+            if (inRange(r[i]-1+dx[j], c[i]-1+dy[j]) && blocks[r[i]-1+dx[j]][c[i]-1+dy[j]] == 1) cnt++;
         }
         if (cnt == 3) cout << 1 << endl;
         else cout << 0 << endl;
