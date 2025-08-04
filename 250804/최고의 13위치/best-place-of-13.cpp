b373bcd512a892c53b50cfac9e16a2de67c548ca
unkn0va <lyunkn0vaa@gmail.com>
2025-08-04 22:09:49 +0900

    Title: 최고의 13위치, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250804/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int grid[20][20];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) cin >> grid[i][j];
+
+    // Please write your code here.
+    int val = INT_MIN, sum = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N-2; j++) {
+            sum = 0;
+            sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+            val = max(val, sum);
+        }
+    }
+
+    cout << val;
+    return 0;
+}
