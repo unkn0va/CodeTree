08bd1d276c2df8080ba8c03e4cc16143baa0108e
unkn0va <lyunkn0vaa@gmail.com>
2025-08-06 13:33:45 +0900

    Title: 오목, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250806/오목/O-mok.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+int board[19][19];
+
+int main() {
+    for (int i = 0; i < 19; i++)
+        for (int j = 0; j < 19; j++) cin >> board[i][j];
+
+    // Please write your code here.
+    int total = 0;
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 19; j++) {
+            if (board[i][j] != 0) {
+                total = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (board[i][j+k] == board[i][j]) total++;
+                    else break;
+
+                    if (total == 5) {
+                        cout << board[i][j] << endl;
+                        cout << i+1 << " " << j+3;
+                    }
+                }
+                total = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (board[i+k][j] == board[i][j]) total++;
+                    else break;
+
+                    if (total == 5) {
+                        cout << board[i][j] << endl;
+                        cout << i+3 << " " << j+1;
+                    }
+                }
+                total = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (board[i+k][j+k] == board[i][j]) total++;
+                    else break;
+
+                    if (total == 5) {
+                        cout << board[i][j] << endl;
+                        cout << i+3 << " " << j+3;
+                    }
+                }
+                total = 0;
+                if (j >= 4) {
+                    for (int k = 0; k < 5; k++) {
+                        if (board[i+k][j-k] == board[i][j]) total++;
+                        else break;
+
+                        if (total == 5) {
+                            cout << board[i][j] << endl;
+                            cout << i+3 << " " << j-1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
