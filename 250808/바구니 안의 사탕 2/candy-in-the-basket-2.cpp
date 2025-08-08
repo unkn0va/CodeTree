a80811847697d19a3ee100ef9e9d2b403ad92216
unkn0va <lyunkn0vaa@gmail.com>
2025-08-08 14:49:52 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250808/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 250808/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -8,7 +8,7 @@ int candy[100] = {}, pos[100] = {};
 
 int main() {
     cin >> N >> K;
-    int max_val = 0, sum, ans = INT_MIN;
+    int max_val = 0, sum = 0, ans = INT_MIN;
     int placed[400] = {};
 
     for (int i = 0; i < N; i++) {
@@ -18,15 +18,24 @@ int main() {
     }
 
     // Please write your code here.
-    for (int i = K; i <= max_val; i++) {
-        sum = 0;
-        for (int j = i-K; j <= i+K; j++) {
-            sum += placed[j];
+    if (max_val < K) {
+        for (int i = 1; i <= max_val; i++) {
+            sum += placed[i];
         }
-        ans = max(sum, ans);
+        cout << sum;
     }
+    else {
+        for (int i = K; i <= max_val; i++) {
+            sum = 0;
+            for (int j = abs(i-K); j <= i+K; j++) {
+                sum += placed[j];
+            }
+            ans = max(sum, ans);
+        }
+        cout << ans;
+    }
+    
 
-    cout << ans;
-
+    
     return 0;
 }
