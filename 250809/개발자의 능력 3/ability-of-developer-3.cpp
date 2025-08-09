11d45205582f148191864e8a298b4dc4f13cbeef
unkn0va <lyunkn0vaa@gmail.com>
2025-08-09 14:42:36 +0900

    Title: 개발자의 능력 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250809/개발자의 능력 3/ability-of-developer-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int abilities[6];
+
+int getDiff(int a, int b, int c) {
+    int sum1 = abilities[a] + abilities[b] + abilities[c];
+    int sum2 = 0;
+    for (int l = 0; l < 6; l++) {
+        sum2 += abilities[l];
+    }
+
+    sum2 -= sum1;
+    return abs(sum1-sum2);
+}
+
+int main() {
+    for (int i = 0; i < 6; i++) {
+        cin >> abilities[i];
+    }
+
+    // Please write your code here.
+    int min_diff = INT_MAX;
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            for (int k = 0; k < 6; k++) {
+                min_diff = min(min_diff, getDiff(i, j, k));
+            }
+        }
+    }
+
+    cout << min_diff;
+
+    return 0;
+}
