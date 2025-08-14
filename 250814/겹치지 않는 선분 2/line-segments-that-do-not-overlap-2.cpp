ad29780a97b08da982a8c5d928ba1e6aaddaa578
unkn0va <lyunkn0vaa@gmail.com>
2025-08-14 19:57:52 +0900

    Title: 겹치지 않는 선분 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250814/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int x1[100], x2[100];
+
+int main() {
+    cin >> N;
+    int a[100] = {}, check[100] = {};
+
+    for (int i = 0; i < N; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    // Please write your code here.
+    int cnt = 0;
+    for (int i = 0; i < N-1; i++) {
+        for (int j = i+1; j < N; j++) {
+            if (x1[i] > x1[j] && x2[i] < x2[j]) {
+                check[i] = 1, check[j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (check[i] == 0) cnt++;
+    }
+    cout << cnt;
+    
+
+    return 0;
+}
