ba1001dbe7b4e19ea729cd5d036a3d5cbc0d9e8e
unkn0va <lyunkn0vaa@gmail.com>
2025-08-14 18:08:08 +0900

    Title: 운행 되고 있는 시간, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250814/운행 되고 있는 시간/hours-in-service.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int N;
+int A[100], B[100];
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+    // Please write your code here.
+    int t = 0, ans = INT_MIN;
+    for (int i = 0; i < N; i++) {
+        int time[100] = {};
+        t = 0;
+        for (int j = 0; j < N; j++) {
+            if (i == j) continue;
+            for (int k = A[j]; k < B[j]; k++) {
+                if (time[k] == 0) {
+                    time[k] = 1;
+                    t++;
+                }
+            }
+        }
+        
+        ans = max(ans, t);
+    }
+
+    cout << ans;
+
+    return 0;
+}
