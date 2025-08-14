5ed88557efe3b9c2ad78ab92dba76a4158b59247
unkn0va <lyunkn0vaa@gmail.com>
2025-08-14 20:41:10 +0900

    Title: 스승의 은혜 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250814/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int N, B;
+int P[1000];
+
+int main() {
+    cin >> N >> B;
+
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+    }
+
+    // Please write your code here.
+    int total = 0, ans = INT_MIN;
+    for (int i = 0; i < N; i++) {
+        total += P[i]/2;
+        if (total > B) continue;
+        int cnt = 1;
+
+        for (int j = 0; j < N; j++) {
+            if (i == j) continue;
+            total += P[j];
+            if (total > B) {
+                total -= P[j];
+                continue;
+            }
+            cnt++;
+        }
+        ans = max(cnt, ans);
+    }
+
+    cout << ans;
+
+    return 0;
+}
