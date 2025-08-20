7e2c38a49b8343a9f5691a8d2566a96579a346b8
unkn0va <lyunkn0vaa@gmail.com>
2025-08-20 15:40:59 +0900

    Title: 데이터센터의 온도 조정 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250820/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
+++ 250820/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -7,13 +7,16 @@ int Ta[1000], Tb[1000];
 
 int main() {
     cin >> N >> C >> G >> H;
+    int min_val = 0, max_val = 0;
     int sum = 0, ans = 0;
     for (int i = 0; i < N; i++) {
         cin >> Ta[i] >> Tb[i];
+        min_val = min(Ta[i], min_val);
+        max_val = max(Tb[i], max_val);
     }
 
     // Please write your code here.
-    for (int i = 3; i < 25; i++) {
+    for (int i = min_val; i <= max_val; i++) {
         sum = 0;
         for (int j = 0; j < N; j++) {
             if (i < Ta[j]) sum += C;
