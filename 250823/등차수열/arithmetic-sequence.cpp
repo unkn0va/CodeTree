92fc1188c0a561943d229699c38f67187d4bc79c
unkn0va <lyunkn0vaa@gmail.com>
2025-08-23 16:27:24 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250823/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int a[100];
+
+int main() {
+    cin >> n;
+    int cnt = 0, min_val = INT_MAX, max_val = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        min_val = min(a[i], min_val);
+        max_val = max(a[i], max_val);
+    }
+
+    // Please write your code here.
+    for (int i = min_val+1; i < max_val; i++) {
+        int num[100] = {};
+        for (int j = 0; j < n; j++) {
+            num[j] = abs(i-a[j]);
+        }
+
+        for (int j = 0; j < n-1; j++) {
+            for (int k = j+1; k < n; k++) {
+                if (num[j] == num[k]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
