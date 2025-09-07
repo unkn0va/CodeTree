0d36d935fdcf40f408f2aa0791ec0178eb2b9f7b
unkn0va <lyunkn0vaa@gmail.com>
2025-09-08 00:16:24 +0900

    Title: 최대 최소간의 차, Time: 1094ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250908/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n, k;
+int arr[100];
+
+int main() {
+    cin >> n >> k;
+    int min_val = INT_MAX, max_val = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        min_val = min(arr[i], min_val);
+        max_val = max(arr[i], max_val);
+    }
+
+    // Please write your code here.
+    int ans = INT_MAX, cost;
+    for (int i = min_val; i <= max_val; i++) {
+        for (int j = max_val; j >= i; j--) {
+            if (j-i > k) continue;
+            cost = 0;
+
+            for (int k = 0; k < n; k++) {
+                if (arr[k] > j) cost += arr[k]-j;
+                if (arr[k] < i) cost += i-arr[k];
+            }
+
+            ans = min(ans, cost);
+        }
+    }
+
+    cout << ans;
+
+
+    return 0;
+}
