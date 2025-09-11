19fe63509655eb710a4868f9d935099ffd6f1604
unkn0va <lyunkn0vaa@gmail.com>
2025-09-11 10:18:50 +0900

    Title: 구역 청소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250911/구역 청소/district-cleaning.cpp
+++ 250911/구역 청소/district-cleaning.cpp
@@ -10,10 +10,10 @@ int main() {
 
     // Please write your code here.
     if (a <= c && b >= d) cout << b-a;
-    else if (b < c) cout << (b-a) + (d-c);
+    else if (b <= c) cout << (b-a) + (d-c);
     else if (a < c && b > c && b < d) cout << (c-a) + (d-c);
     else if (c <= a && d >= b) cout << d-c;
-    else if (d < a) cout << (d-c) + (b-a);
+    else if (d <= a) cout << (d-c) + (b-a);
     else if (c < a && d > a && d < b) cout << (a-c) + (b-a);
 
     return 0;
