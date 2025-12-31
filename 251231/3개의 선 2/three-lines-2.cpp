04952a8991110f8be379c9247d765ed8c745ae75
unkn0va <lyunkn0vaa@gmail.com>
2025-12-31 16:37:45 +0900

    Title: 3개의 선 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251231/3개의 선 2/three-lines-2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int x[20], y[20];
+
+bool check(int a, int b, int c) {
+    bool is_deleted[21] = {false};
+    int type[3] = {a, b, c};
+    int first_idx = -1;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!is_deleted[j]) {
+                first_idx = j;
+                break; 
+            }
+        }
+
+        if (first_idx = -1) return true;
+
+        int t_x = x[first_idx], t_y = y[first_idx];
+
+        for (int j = 0; j < n; j++) {
+            if (type[i] == 0) { // 세로선
+                if (x[j] == t_x) is_deleted[j] = false;
+            }
+            else { // 가로선
+                if (y[j] == t_y) is_deleted[j] = false;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (!is_deleted[j]) return false;
+        }
+
+        return true;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    // Please write your code here.
+    bool possible = false;
+    for (int i = 0; i < 8; i++) {
+        if (check(i >> 2 & 1, i >> 1 & 1, i & 1)) {
+            possible = true;
+            break;
+        }
+    }
+
+    if (possible) cout << 1;
+    else cout << 0;
+    
+    return 0;
+}
