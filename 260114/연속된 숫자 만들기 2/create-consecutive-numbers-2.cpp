2e8b577dc96ae3948b66d8f6c64eb8d5db3d850d
unkn0va <lyunkn0vaa@gmail.com>
2026-01-14 18:09:27 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260114/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 260114/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -12,16 +12,18 @@ int main() {
     // case 1: 오름차순
     if (pos[0] < pos[1] && pos[0] < pos[2] && pos[1] < pos[2]) {
         if (pos[2] - pos[0] == 2) cout << 0;
+        else if (pos[1] - pos[0] == 1 || pos[2] - pos[1] == 1) cout << 2;
         else if (pos[2] - pos[1] > 2 && pos[1] - pos[0] > 2) cout << 2;
         else cout << 1;
     }
     // case 2: 내림차순
     else if (pos[0] > pos[1] && pos[0] > pos[2] && pos[1] > pos[2]) {
         if (pos[0] - pos[2] == 2) cout << 0;
+        else if (pos[0] - pos[1] == 1 || pos[1] - pos[2] == 1) cout << 2;
         else if (pos[0] - pos[1] > 2 && pos[1] - pos[2] > 2) cout << 2;
         else cout << 1;
     }
-    // case 3: 6 2 9 / 9 2 6 / 6 9 3 / 9 5 7
+    // case 3: 6 2 9 / 9 2 6 / 6 9 3 / 9 5 7 / 6 5 9
     else {
         if (abs(pos[1] - pos[0]) == 2) cout << 1;
         else if (abs(pos[2] - pos[1]) == 2) cout << 1;
