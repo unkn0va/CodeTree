b2df7c1ebb95c992f81cd170be95b2abff398e4e
unkn0va <lyunkn0vaa@gmail.com>
2026-02-02 13:23:44 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260202/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+char board[10][10];
+
+int main() {
+    int idx_r, idx_c, i_r, i_c, r, c;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 'L') {
+                idx_r = i;
+                idx_c = j;
+            }
+            else if (board[i][j] == 'R') {
+                i_r = i;
+                i_c = j;
+            }
+            else if (board[i][j] == 'B') {
+                r = i;
+                c = j;
+            }
+        }
+    }
+
+    // Please write your code here.
+    cout << idx_r-r + abs(idx_c-c) - 1;
+
+    return 0;
+}
