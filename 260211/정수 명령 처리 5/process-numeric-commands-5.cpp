a358db62e3f5595d930057af1e946f655aa35540
unkn0va <lyunkn0vaa@gmail.com>
2026-02-11 13:26:36 +0900

    Title: 정수 명령 처리 5, Time: 12ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260211/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+
+int main() {
+    cin >> N;
+    vector<int> v;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+        }
+        if (command[i] == "push_back") {
+            v.push_back(num[i]);
+        }
+        else if (command[i] == "get") {
+            cout << v[num[i]-1] << "\n";
+        }
+        else if (command[i] == "size") {
+            cout << v.size() << "\n";
+        }
+        else if (command[i] == "pop_back") {
+            v.pop_back();
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
